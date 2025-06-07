9ff1f64251f6f79856ce841a25642f47d73a5bb0
dvlpsy <adgjl0122@naver.com>
2025-06-07 15:18:29 +0900

    [feat #17-trade] 거래 Service 생성

---
+++ trade/src/main/java/com/bookstore/trade/application/service/v1/TradeServiceApiV1Impl.java
@@ -0,0 +1,70 @@
+package com.bookstore.trade.application.service.v1;
+
+import com.bookstore.trade.application.dto.v1.request.ReqTradeCancelDtoApiV1;
+import com.bookstore.trade.application.dto.v1.request.ReqTradeRequestDtoApiV1;
+import com.bookstore.trade.application.dto.v1.response.ResTradeAcceptDtoApiV1;
+import com.bookstore.trade.application.dto.v1.response.ResTradeCancelDtoApiV1;
+import com.bookstore.trade.application.dto.v1.response.ResTradeCompleteDtoApiV1;
+import com.bookstore.trade.application.dto.v1.response.ResTradeGetDetailListDtoApiV1;
+import com.bookstore.trade.application.dto.v1.response.ResTradeRequestDtoApiV1;
+import com.bookstore.trade.domain.trade.entity.TradeEntity;
+import com.bookstore.trade.domain.trade.repository.TradeRepository;
+import com.bookstore.trade.domain.trade.vo.TradeStatus;
+import jakarta.transaction.Transactional;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class TradeServiceApiV1Impl implements TradeServiceApiV1 {
+
+	private final TradeRepository tradeRepository;
+
+	@Override
+	public ResTradeRequestDtoApiV1 postTradeRequest(
+		ReqTradeRequestDtoApiV1 reqTradeRequestDtoApiV1) {
+		TradeEntity tradeEntity = tradeRepository.save(reqTradeRequestDtoApiV1.getTrade().toEntity());
+		return ResTradeRequestDtoApiV1.of(tradeEntity);
+	}
+
+	@Override
+	public ResTradeAcceptDtoApiV1 postTradeAccept(UUID id) {
+		TradeEntity tradeEntity = tradeRepository.findById(id)
+			.orElseThrow(() -> new RuntimeException("Trade not found"));
+		updateTradeState(id, "ACCEPTED");
+		return ResTradeAcceptDtoApiV1.of(tradeEntity);
+	}
+
+	@Override
+	public ResTradeCancelDtoApiV1 postTradeCancel(UUID id,
+		ReqTradeCancelDtoApiV1 reqTradeCancelDtoApiV1) {
+		TradeEntity tradeEntity = tradeRepository.findById(id)
+			.orElseThrow(() -> new RuntimeException("Trade not found"));
+		updateTradeState(id, "CANCELED");
+		return ResTradeCancelDtoApiV1.of(tradeEntity);
+	}
+
+	@Override
+	public ResTradeCompleteDtoApiV1 postTradeComplete(UUID id) {
+		TradeEntity tradeEntity = tradeRepository.findById(id)
+			.orElseThrow(() -> new RuntimeException("Trade not found"));
+		updateTradeState(id, "COMPLETED");
+		return ResTradeCompleteDtoApiV1.of(tradeEntity);
+	}
+
+	@Override
+	public ResTradeGetDetailListDtoApiV1 getTradeDetailList(UUID id) {
+		TradeEntity tradeEntity = tradeRepository.findById(id)
+			.orElseThrow(() -> new RuntimeException("Trade not found"));
+		return ResTradeGetDetailListDtoApiV1.of(tradeEntity);
+	}
+
+	private void updateTradeState(UUID id, String status) {
+		TradeEntity tradeEntity = tradeRepository.findById(id)
+			.orElseThrow(() -> new RuntimeException("Trade not found"));
+		TradeStatus tradeStatus = TradeStatus.valueOf(status);
+		tradeEntity.updateStatus(tradeStatus);
+	}
+}
