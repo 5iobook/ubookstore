8e98f80bb84fe6a9b5dabbb94f2f43b3d2c604cd
dvlpsy <adgjl0122@naver.com>
2025-06-07 15:17:37 +0900

    [feat #17-trade] 거래 Request Dto 생성

---
+++ trade/src/main/java/com/bookstore/trade/application/dto/v1/request/ReqTradeCancelDtoApiV1.java
@@ -0,0 +1,34 @@
+package com.bookstore.trade.application.dto.v1.request;
+
+import com.bookstore.trade.domain.trade.entity.TradeEntity;
+import com.bookstore.trade.domain.trade.vo.TradeStatus;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReqTradeCancelDtoApiV1 {
+
+	private Trade trade;
+
+	@Getter
+	@Builder
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class Trade {
+		private String cancelReason;
+		private TradeStatus status = TradeStatus.CANCELED;
+
+		public TradeEntity toEntity() {
+			return TradeEntity.builder()
+				.cancelReason(cancelReason)
+				.status(status)
+				.build();
+		}
+	}
+}
