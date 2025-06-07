cc1d9bc05a733f355c146c1cbb75705a2c502a4d
dvlpsy <adgjl0122@naver.com>
2025-06-07 15:16:42 +0900

    [feat #17-trade] 거래 Entity, Repository 생성

---
+++ trade/src/main/java/com/bookstore/trade/domain/trade/entity/TradeEntity.java
@@ -0,0 +1,51 @@
+package com.bookstore.trade.domain.trade.entity;
+
+import com.bookstore.trade.domain.trade.vo.TradeMethod;
+import com.bookstore.trade.domain.trade.vo.TradeStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "p_trade")
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TradeEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private UUID id;
+	@Column(name= "buyer_id")
+	private UUID buyerId; // 구매자 nullable = false
+	@Column(name= "seller_id")
+	private UUID sellerId; // 판매자 nullable = false
+	@Column(name= "post_id")
+	private UUID postId; // 연결된 판매글 nullable = false
+	@Enumerated(EnumType.STRING)
+	@Column
+	private TradeStatus status; // REQUESTED, ACCEPTED, IN_PROGRESS, COMPLETED, CANCELED, nullable = false
+	@Column
+	private TradeMethod method; // DIRECT, DELIVERY, nullable = false
+	@Column(name= "meet_up_location", columnDefinition = "TEXT")
+	private String meetUpLocation; // 거래 장소 nullable = false
+	@Column(name= "cancel_reason", columnDefinition = "TEXT")
+	private String cancelReason;
+	@Column(name= "completed_at")
+	private LocalDateTime completedAt; // 거래 완료 시간
+
+	public void updateStatus(TradeStatus status) {
+		this.status = status;
+	}
+}
