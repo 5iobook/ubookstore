ba5dc7d93d66fdc6c1066cef836790dcc0c0dcc3
문고은 <>
2025-06-11 01:01:12 +0900

    [feat #27-post] PostServiceApiV1 구현 (게시글 CRUD)

---
+++ post/src/main/java/com/bookstore/post/post/application/service/v1/PostServiceApiV1.java
@@ -0,0 +1,23 @@
+package com.bookstore.post.post.application.service.v1;
+
+import com.bookstore.post.post.application.dto.v1.request.ReqPostPostDTOApiV1;
+import com.bookstore.post.post.application.dto.v1.request.ReqPostPutDTOApiV1;
+import com.bookstore.post.post.application.dto.v1.response.ResPostGetByIdDTOApiV1;
+import com.bookstore.post.post.application.dto.v1.response.ResPostGetDTOApiV1;
+import com.bookstore.post.post.application.dto.v1.response.ResPostPostDTOApiV1;
+import com.bookstore.post.post.application.dto.v1.response.ResPostPutDTOApiV1;
+import com.querydsl.core.types.Predicate;
+import java.util.UUID;
+import org.springframework.data.domain.Pageable;
+
+public interface PostServiceApiV1 {
+    ResPostPostDTOApiV1 postBy(ReqPostPostDTOApiV1 dto);
+
+    ResPostGetByIdDTOApiV1 getBy(UUID id);
+
+    ResPostGetDTOApiV1 getBy(Predicate predicate, Pageable pageable);
+
+    ResPostPutDTOApiV1 putBy(UUID id, ReqPostPutDTOApiV1 dto);
+
+    void deleteBy(UUID id);
+}
