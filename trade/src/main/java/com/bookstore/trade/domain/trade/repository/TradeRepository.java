3c8fb9de73317e2eed6e6aa1e88d0bcb5c229f92
dvlpsy <adgjl0122@naver.com>
2025-06-19 08:49:24 +0900

    [feat #45-trade] common 연동 및 검색 API 추가 (entity, repository)

--- trade/src/main/java/com/bookstore/trade/domain/trade/repository/TradeRepository.java
+++ trade/src/main/java/com/bookstore/trade/domain/trade/repository/TradeRepository.java
@@ -1,9 +1,14 @@
 package com.bookstore.trade.domain.trade.repository;
 
 import com.bookstore.trade.domain.trade.entity.TradeEntity;
+import com.querydsl.core.types.Predicate;
+import java.util.Optional;
 import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
-public interface TradeRepository extends JpaRepository<TradeEntity, UUID> {
-
+public interface TradeRepository {
+	TradeEntity save(TradeEntity trade);
+	Optional<TradeEntity> findById(UUID id);
+	Page<TradeEntity> findAll(Predicate predicate, Pageable pageable);
 }
