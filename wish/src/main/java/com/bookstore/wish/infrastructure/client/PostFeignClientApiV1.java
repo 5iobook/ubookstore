b1687489a80935694866a1b32ad49b33c45de269
문고은 <>
2025-06-20 19:40:24 +0900

    [feat #52-post-wish] 게시글 찜 수 증감 요청 Feign Client 구현

--- wish/src/main/java/com/bookstore/wish/infrastructure/client/PostFeignClientApiV1.java
+++ wish/src/main/java/com/bookstore/wish/infrastructure/client/PostFeignClientApiV1.java
@@ -2,14 +2,20 @@ package com.bookstore.wish.infrastructure.client;
 
 import java.util.UUID;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(name = "postClient", url = "http://localhost:8081")
 public interface PostFeignClientApiV1 {
 
-    @GetMapping("/v1/posts/{id}/exists")
-    ResponseEntity<Void> existsBy(@PathVariable UUID id);
+    @GetMapping("/v1/internal/posts/{id}/exists")
+    void existsBy(@PathVariable UUID id);
+
+    @PostMapping("/v1/internal/posts/{id}/wish-count/increase")
+    void increaseWishCount(@PathVariable UUID id);
+
+    @PostMapping("/v1/internal/posts/{id}/wish-count/decrease")
+    void decreaseWishCount(@PathVariable UUID id);
 
 }
