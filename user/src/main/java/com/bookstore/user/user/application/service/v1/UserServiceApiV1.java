4aa5d51996b2e15a52c15b9ef0aaa6e86030dda9
syn <tlsdpsk1992@naver.c0m>
2025-06-23 16:46:27 +0900

    [feat] UserServiceApiV1 로그인 메서드 생성

--- user/src/main/java/com/bookstore/user/user/application/service/v1/UserServiceApiV1.java
+++ user/src/main/java/com/bookstore/user/user/application/service/v1/UserServiceApiV1.java
@@ -1,9 +1,13 @@
 package com.bookstore.user.user.application.service.v1;
 
+import com.bookstore.user.user.application.dto.v1.req.ReqUserPostSigninDtoApiV1;
 import com.bookstore.user.user.application.dto.v1.req.ReqUserPostSignupDtoApiV1;
+import com.bookstore.user.user.application.dto.v1.res.ResTokenDtoApiV1;
 
 public interface UserServiceApiV1 {
 
     void signUp(ReqUserPostSignupDtoApiV1 dto);
+
+    ResTokenDtoApiV1 signIn(ReqUserPostSigninDtoApiV1 dto);
 
 }
