4699b94dfc562d0e451d13209c4cc314372f85ae
syn <tlsdpsk1992@naver.c0m>
2025-07-26 00:34:51 +0900

    [feat] Refreeshtoken 레포지토리 메서드 추가

--- user/src/main/java/com/bookstore/user/user/domain/repository/RefreshTokenRepository.java
+++ user/src/main/java/com/bookstore/user/user/domain/repository/RefreshTokenRepository.java
@@ -1,7 +1,11 @@
 package com.bookstore.user.user.domain.repository;
 
+import com.bookstore.user.user.domain.entity.RefreshTokenEntity;
+import java.util.Optional;
+
 public interface RefreshTokenRepository {
 
+    Optional<RefreshTokenEntity> findByUser_Email(String email);
 
-
+    RefreshTokenEntity save(RefreshTokenEntity tokenEntity);
 }
--- user/src/main/java/com/bookstore/user/user/infrastructure/repository/RefreshTokenJpaRepository.java
+++ user/src/main/java/com/bookstore/user/user/infrastructure/repository/RefreshTokenJpaRepository.java
@@ -1,9 +1,7 @@
 package com.bookstore.user.user.infrastructure.repository;
 
 import com.bookstore.user.user.domain.entity.RefreshTokenEntity;
-import com.bookstore.user.user.domain.entity.UserEntity;
 import com.bookstore.user.user.domain.repository.RefreshTokenRepository;
-import com.bookstore.user.user.domain.repository.UserRepository;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
