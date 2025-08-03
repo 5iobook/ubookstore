c97645276d8f5dd43bc704c27732b003c96bb5b6
syn <tlsdpsk1992@naver.c0m>
2025-08-03 20:05:32 +0900

    [feat] 내 정보 응답 dto 생성

---
+++ user/src/main/java/com/bookstore/user/user/application/dto/v1/res/ResMyuserInfoDtoApiV1.java
@@ -0,0 +1,23 @@
+package com.bookstore.user.user.application.dto.v1.res;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class ResMyuserInfoDtoApiV1 {
+    private ResMyuserInfoDtoApiV1.User user;
+
+    @Builder
+    @Getter
+    public static class User{
+        private String userName;
+
+        private String nickName;
+
+        private String email;
+
+        private String profile;
+    }
+
+}
