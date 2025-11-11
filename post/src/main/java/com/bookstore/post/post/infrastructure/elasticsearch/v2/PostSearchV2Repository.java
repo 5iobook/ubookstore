98258fb289e866031af1153b5b6f4c606b058e96
adgjl <adgjl0122@naver.com>
2025-11-11 18:37:24 +0900

    [feat] Post 도메인 ELK 인프라 설정 추가

---
+++ post/src/main/java/com/bookstore/post/post/infrastructure/elasticsearch/v2/PostSearchV2Repository.java
@@ -0,0 +1,11 @@
+package com.bookstore.post.post.infrastructure.elasticsearch.v2;
+
+import com.bookstore.post.post.entity.v2.PostV2;
+//import org.springframework.?data.elasticsearch.repository.ElasticsearchRepository;
+
+import java.util.List;
+
+//public interface PostSearchV2Repository extends ElasticsearchRepository<PostV2, String> {
+    // 커스텀 쿼리 메서드 정의
+//    List<PostV2> findByTitleContaining(String title);
+//}
